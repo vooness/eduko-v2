fca255184e5acd933c3b7e01b14f471263173f1c
vooness <vooness@stream.cz>
2025-04-02 11:59:42 +0200

    middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -5,41 +5,24 @@ export function middleware(req: NextRequest) {
   console.log("Middleware initialized");
 
   const url = req.nextUrl;
-  const tokenFromUrl = url.searchParams.get("token");
 
-  // Token uložený v cookies (pokud existuje)
-  const tokenFromCookies = req.cookies.get("token")?.value;
-
-  // Platný token
-  const validToken = "special123";
-
-  // Pokud uživatel už je na /pristup-odepren, nepřesměrovávat znovu
+  // Pokud už je uživatel na stránce s odepřeným přístupem, nepřesměrovávat znovu
   if (url.pathname.startsWith("/Pristup-odepren")) {
     return NextResponse.next();
   }
 
-  // Pokud token v cookies nebo URL odpovídá platnému tokenu
-  if (tokenFromUrl === validToken || tokenFromCookies === validToken) {
-    const response = NextResponse.next();
+  // Získání hlavičky referer
+  const referer = req.headers.get("referer") || "";
 
-    // Pokud token pochází z URL a není v cookies, uložíme jej
-    if (tokenFromUrl === validToken && tokenFromCookies !== validToken) {
-      response.cookies.set("token", tokenFromUrl, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        path: "/",
-      });
-      console.log("Token uložen do cookies");
-    }
-
-    return response;
+  // Kontrola, zda referer začíná na https://online.flexibooks.cz
+  if (!referer.startsWith("https://online.flexibooks.cz")) {
+    return NextResponse.redirect(new URL("/Pristup-odepren", req.url));
   }
 
-  // Přesměrování na stránku s odepřením přístupu
-  return NextResponse.redirect(new URL("/Pristup-odepren", req.url));
+  return NextResponse.next();
 }
 
-// Konfigurace matcheru
+// Konfigurace matcheru pro všechny stránky kromě API, _next a favicon.ico
 export const config = {
-  matcher: ["/((?!api|_next|favicon.ico).*)"], // Middleware pro všechny stránky kromě specifikovaných
+  matcher: ["/((?!api|_next|favicon.ico).*)"],
 };
