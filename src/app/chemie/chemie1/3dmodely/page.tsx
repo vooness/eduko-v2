54f1a815f2d486be20c217322e6af23a85b4df09
vooness <vooness@stream.cz>
2025-05-18 21:19:34 +0200

    3dmodely

--- src/app/chemie/chemie1/3dmodely/page.tsx
+++ src/app/chemie/chemie1/3dmodely/page.tsx
@@ -4,67 +4,302 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { useRouter } from "next/navigation";
 
-const modelsData = [
-  {
-    name: "Interaktivní 3D model",
-    description: "Popis 3D modelu...", // Nepovinný popis
-    path: "/chemie/chemie1/3dmodely/3dmodel1", // Opravená absolutní cesta
+const modelsData = [   
+  {     
+    name: "Interaktivní 3D model",     
+    description: "Popis 3D modelu...", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/3dmodel1", // Opravená absolutní cesta   
+  },   
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s019 2.1_oxid uhličitý kalot", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s019_2.1_oxid_uhlicity_kalot", // Opravená absolutní cesta   
+  },    
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.1.1_Orbital_1s", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.1.1_Orbital_1s", // Opravená absolutní cesta   
+  },    
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.1.2_Orbital_2s", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.1.2_Orbital_2s", // Opravená absolutní cesta   
+  },    
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.1.3_Orbital_3s", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.1.3_Orbital_3s", // Opravená absolutní cesta   
+  },    
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.2.1_Orbital_2p_y", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.2.1_Orbital_2p_y", // Opravená absolutní cesta   
+  },    
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.2.2_Orbital_2p_z", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.2.2_Orbital_2p_z", // Opravená absolutní cesta   
+  },    
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.2.3_Orbital_2p_x", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.2.3_Orbital_2p_x", // Opravená absolutní cesta   
+  },    
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.2.4_podslupka_2p", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.2.4_podslupka_2p", // Opravená absolutní cesta   
+  },    
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.3.1_Orbital_3d_xy", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.3.1_Orbital_3d_xy", // Opravená absolutní cesta   
+  },    
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035_3.2.3.2_Orbital_3d_xz", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.3.2_Orbital_3d_xz", // Opravená absolutní cesta   
+  },    
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035_3.2.3.3_Orbital_3d_z2", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.3.3_Orbital_3d_z2", // Opravená absolutní cesta   
   },
-  {
-    name: "Interaktivní 3D model",
-    description: "s019 2.1_oxid uhličitý kalot", // Nepovinný popis
-    path: "/chemie/chemie1/3dmodely/3dmodel2", // Opravená absolutní cesta
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.3.4_Orbital_3d_yz", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.3.4_Orbital_3d_yz", // Opravená absolutní cesta   
   },
-   {
-    name: "Interaktivní 3D model",
-    description: "s035 3.2.1.1_Orbital_1s", // Nepovinný popis
-    path: "/chemie/chemie1/3dmodely/3dmodel3", // Opravená absolutní cesta
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.3.5_Orbital_3d_x2_y2", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.3.5_Orbital_3d_x2_y2", // Opravená absolutní cesta   
   },
-   {
-    name: "Interaktivní 3D model",
-    description: "s035 3.2.1.2_Orbital_2s", // Nepovinný popis
-    path: "/chemie/chemie1/3dmodely/3dmodel4", // Opravená absolutní cesta
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.3.6_podslupka_3d", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.3.6_podslupka_3d", // Opravená absolutní cesta   
   },
-   {
-    name: "Interaktivní 3D model",
-    description: "s035 3.2.1.3_Orbital_3s", // Nepovinný popis
-    path: "/chemie/chemie1/3dmodely/3dmodel5", // Opravená absolutní cesta
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.4.1_orbital_4f_-3", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.4.1_orbital_4f_-3", // Opravená absolutní cesta   
   },
-   {
-    name: "Interaktivní 3D model",
-    description: "s035 3.2.2.1_Orbital_2p_y", // Nepovinný popis
-    path: "/chemie/chemie1/3dmodely/3dmodel6", // Opravená absolutní cesta
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.4.2_orbital_4f_-2", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.4.2_orbital_4f_-2", // Opravená absolutní cesta   
   },
-   {
-    name: "Interaktivní 3D model",
-    description: "s035 3.2.2.2_Orbital_2p_z", // Nepovinný popis
-    path: "/chemie/chemie1/3dmodely/3dmodel7", // Opravená absolutní cesta
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.4.3_orbital_4f_-1", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.4.3_orbital_4f_-1", // Opravená absolutní cesta   
   },
-   {
-    name: "Interaktivní 3D model",
-    description: "s035 3.2.2.3_Orbital_2p_x", // Nepovinný popis
-    path: "/chemie/chemie1/3dmodely/3dmodel8", // Opravená absolutní cesta
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.4.4_orbital_4f_0", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.4.4_orbital_4f_0", // Opravená absolutní cesta   
   },
-   {
-    name: "Interaktivní 3D model",
-    description: "s035 3.2.2.4_podslupka_2p", // Nepovinný popis
-    path: "/chemie/chemie1/3dmodely/3dmodel9", // Opravená absolutní cesta
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.4.5_orbital_4f_1", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.4.5_orbital_4f_1", // Opravená absolutní cesta   
   },
-   {
-    name: "Interaktivní 3D model",
-    description: "s035 3.2.3.1_Orbital_3d_xy", // Nepovinný popis
-    path: "/chemie/chemie1/3dmodely/3dmodel10", // Opravená absolutní cesta
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.4.6_orbital_4f_2", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.4.6_orbital_4f_2", // Opravená absolutní cesta   
   },
-   {
-    name: "Interaktivní 3D model",
-    description: "Popis 3D modelu...", // Nepovinný popis
-    path: "/chemie/chemie1/3dmodely/3dmodel1", // Opravená absolutní cesta
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.4.7_orbital_4f_3", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.4.7_orbital_4f_3", // Opravená absolutní cesta   
   },
-   {
-    name: "Interaktivní 3D model",
-    description: "Popis 3D modelu...", // Nepovinný popis
-    path: "/chemie/chemie1/3dmodely/3dmodel1", // Opravená absolutní cesta
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s035 3.2.4.8_podslupka_4f", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s035_3.2.4.8_podslupka_4f", // Opravená absolutní cesta   
   },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s046 4.01_krystalova_mrizka", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s046_4.01_krystalova_mrizka", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s048 4.02.2.0_tvar_linearni_A", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s048_4.02.2.0_tvar_linearni_A", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s048 4.02.3.0_tvar_trigonalni", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s048_4.02.3.0_tvar_trigonalni", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s048 4.02.3.1_tvar_lomeny_A", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s048_4.02.3.1_tvar_lomeny_A", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s048 4.02.4.0_tvar_tetraedr", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s048_4.02.4.0_tvar_tetraedr", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s048 4.02.4.1_tvar_trigonalni_pyramida", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s048_4.02.4.1_tvar_trigonalni_pyramida", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s048 4.02.4.2_tvar_lomeny_B", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s048_4.02.4.2_tvar_lomeny_B", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s049 4.02.5.0_tvar_trigonalni_bipyramida", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s049_4.02.5.0_tvar_trigonalni_bipyramida", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s049 4.02.5.1_tvar_houpacka", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s049_4.02.5.1_tvar_houpacka", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s049 4.02.5.2_tvar_T", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s049_4.02.5.2_tvar_T", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s049 4.02.5.3_tvar_linearni_B", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s049_4.02.5.3_tvar_linearni_B", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s049 4.02.6.0_tvar_oktaedr", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s049_4.02.6.0_tvar_oktaedr", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s049 4.02.6.1_tvar_tetragonalni_pyramida", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s049_4.02.6.1_tvar_tetragonalni_pyramida", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s049 4.02.6.2_tvar_tetragonalni", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s049_4.02.6.2_tvar_tetragonalni", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s053 4.03a_prekryv_s-s", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s053_4.03a_prekryv_s-s", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s053 4.03b_prekryv_s-p", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s053_4.03b_prekryv_s-p", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s053 4.03c_prekryv_sp-s", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s053_4.03c_prekryv_sp-s", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s054 4.05.1_prekryv_p-p", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s054_4.05.1_prekryv_p-p", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s054 4.05.2_prekryv_pi", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s054_4.05.2_prekryv_pi", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s054 4.06.2_vazba_CO2", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s054_4.06.2_vazba_CO2", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s054 4.04_voda", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s054_4.04_voda", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s055 4.07_dusik", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s055_4.07_dusik", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s057 4.10_struktura_diamantu2", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s057_4.10_struktura_diamantu2", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s058 4.11_struktura_ledu", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s058_4.11_struktura_ledu", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s058 4.12_struktura_grafitu2", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s058_4.12_struktura_grafitu2", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s096 6.04_Alkalicka_baterie", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s096_6.04_Alkalicka_baterie", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s104 7.01_graf_atom_polomery", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s104_7.01_graf_atom_polomery", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s105 7.02_graf_atom_ion_energie", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s105_7.02_graf_atom_ion_energie", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s117 8.01_dikyslik_a_ozon", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s117_8.01_dikyslik_a_ozon", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s119 8.02_molekula_vody", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s119_8.02_molekula_vody", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s120 8.03_peroxid_vodiku", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s120_8.03_peroxid_vodiku", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s122 8.04_sira_8", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s122_8.04_sira_8", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s129 8.05_kyselina_dusicna", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s129_8.05_kyselina_dusicna", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s129 8.06_bily_fosfor", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s129_8.06_bily_fosfor", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s131 8.07_fulleren", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s131_8.07_fulleren", // Opravená absolutní cesta   
+  },
+  {     
+    name: "Interaktivní 3D model",     
+    description: "s137 kremicitany", // Nepovinný popis     
+    path: "/chemie/chemie1/3dmodely/s137_kremicitany", // Opravená absolutní cesta   
+  }
 ];
 
 export default function Model3DPage() {
